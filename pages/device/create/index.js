df7e55cc19c66750bc6388a0d530bea22d9f553f
Man, Shuo <shuo.man@ge.com>
2017-04-07 22:50:17 +0800

    choose image, but doesn't work in mobile

--- pages/device/create/index.js
+++ pages/device/create/index.js
@@ -2,29 +2,57 @@
 import { scan } from '../../../actions/native'
 Page({
   data:{
+    imageList: [],
+    sourceTypeIndex: 2,
+    sourceType: ['拍照', '相册', '拍照或相册'],
+
+    sizeTypeIndex: 2,
+    sizeType: ['压缩', '原图', '压缩或原图'],
+
+    countIndex: 8,
+    count: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  
     btnlabel: 'Load',
     qrCode: '',
     status: '',
     sites: []
   },
-  eventScan: function() {
-    this.setData({ status: 'Scanning...' })
-    wx.scanCode({
-      success: (res) => {
-        this.setData({
-          status: JSON.stringify(res)
-        })
-      },
-      fail: (res) => {
-        this.setData({
-          status: JSON.stringify(res)
-        })
-      },
-      complete: (res) => {
-        this.setData({
-          status: JSON.stringify(res)
+  sourceTypeChange: function (e) {
+    this.setData({
+      sourceTypeIndex: e.detail.value
+    })
+  },
+  sizeTypeChange: function (e) {
+    this.setData({
+      sizeTypeIndex: e.detail.value
+    })
+  },
+  countChange: function (e) {
+    this.setData({
+      countIndex: e.detail.value
+    })
+  },
+  chooseImage: function () {
+    console.log('choose Image...')
+    var that = this
+    wx.chooseImage({
+      sourceType: 2,
+      sizeType: 2,
+      count: this.data.count[this.data.countIndex],
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          imageList: res.tempFilePaths
         })
       }
+    })
+  },
+  previewImage: function (e) {
+    var current = e.target.dataset.src
+
+    wx.previewImage({
+      current: current,
+      urls: this.data.imageList
     })
   },
   eventPost: function() {
