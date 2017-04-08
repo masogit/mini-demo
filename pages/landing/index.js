7555367cfd6f9e253165e74d5b100d8a93e18150
Man, Shuo <shuo.man@ge.com>
2017-04-08 18:42:02 +0800

    update

--- pages/landing/index.js
+++ pages/landing/index.js
@@ -18,6 +18,33 @@ Page({
 
   onLoad() {
 
+    rest.go({}, wx.login)
+      .then(res => res.code, err => console.log('err'))
+      // .then(code => rest.go({ method: 'GET', url: urls.getOpenId + user.code }))
+      // .then(res => res.data.openid)
+      // .then(openid => {
+      //   getApp().globalData.weChatId = openid
+
+      //   return rest.go({
+      //     method: 'POST',
+      //     url: urls.weChatAuth,
+      //     data: { weChatId: openid }
+      //   })
+      // })
+      // .then(res => res.data.data.loginName)
+      // .then(loginName => {
+      //   getApp().globalData.loginName = loginName
+      //   return rest.go({}, wx.getUserInfo)
+      // })
+      // .then(res => res.userInfo)
+      // .then(userInfo => {
+      //   this.setStatus('got userInfo: ' + userInfo)
+      //   this.setData({ userInfo })
+      //   getApp().globalData.userInfo = userInfo
+      //   wx.redirectTo({ url: '/pages/index' })
+      // }, err => { console.log(err); wx.redirectTo({ url: '/pages/user/login/index' })})
+
+
     wx.login({
     success: res => {
 
