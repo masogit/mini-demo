68e0e763262a5df2b046994ab971798b2ee091d2
Man, Shuo <shuo.man@ge.com>
2017-04-09 01:40:06 +0800

    refine promise then chain

--- pages/landing/index.js
+++ pages/landing/index.js
@@ -11,25 +11,24 @@ Page({
 
   onLoad() {
 
-    // 微信登录 > 取openid > 自动登录APM, 保存openid, 保存用户资料, 用户名
+    /**
+     * 微信登录 > 取openid > 自动登录APM, 保存openid, 保存用户资料, 用户名
+     **/
     rest.go({}, wx.login)
-      .then(res => res.code) // 登录微信, 取code
-      .then(code => rest.go({ method: 'GET', url: urls.getOpenId + code })) // 取openid
-      .then(res => res.data.openid) // 获得openid
-      .then(openid => { 
-        getApp().globalData.weChatId = openid // 存openid
-        return rest.go({ method: 'POST', url: urls.weChatAuth, data: { weChatId: openid } }) }) // 验证openid在APM
-      .then(res => res.data.data.loginName) // 如果存在APM, 取loginName; 否则登录页面
-      .then(loginName => { 
-        getApp().globalData.loginName = loginName; // 存APM登录名
-        return rest.go({}, wx.getUserInfo) }) // 取微信用户资料
-      .then(res => res.userInfo)  // 获得微信用户资料
-      .then(userInfo => {
-        this.setData({ userInfo })  // 显示用户资料
-        getApp().globalData.userInfo = userInfo // 保存用户资料
-        
-        wx.redirectTo({ url: '/pages/index' }) }  // 存GlobalData, 并跳转首页
-      ,err => { console.log(err); wx.redirectTo({ url: '/pages/user/login/index' })})  // 失败跳转登录页面
-
+      .then(res => res.code)                                                                          // 登录微信, 取code
+      .then(code => rest.go({ method: 'GET', url: urls.getOpenId + code }))                           // 取openid
+      .then(res => res.data.openid)                                                                   // 获得openid
+      .then(openid => { getApp().globalData.weChatId = openid; return openid })                       // 存openid
+      .then(openid => rest.go({ method: 'POST', url: urls.weChatAuth, data: { weChatId: openid } }))  // 验证openid在APM
+      .then(res => res.data.data.loginName)                                                           // 如果存在APM, 取loginName; 否则登录页面
+      .then(loginName => getApp().globalData.loginName = loginName)                                   // 存APM登录名
+      .then(() => rest.go({}, wx.getUserInfo))                                                        // 取微信用户资料
+      .then(res => res.userInfo)                                                                      // 获得微信用户资料
+      .then(userInfo => { this.setData({ userInfo }); return userInfo })                              // 显示用户资料
+      .then(userInfo => getApp().globalData.userInfo = userInfo)                                      // 保存用户资料
+      .then(
+        () => wx.redirectTo({ url: '/pages/index' })                                                  // 存GlobalData, 并跳转首页
+        ,err => { console.log(err); wx.redirectTo({ url: '/pages/user/login/index' })}                // 失败跳转登录页面
+      )
   }
 })
